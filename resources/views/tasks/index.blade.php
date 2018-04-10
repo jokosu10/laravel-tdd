22c1cbebb8ad5cee2efaa030f8e893e4c1fa65e8
Joko Susilo <susilo.j8@gmail.com>
2018-04-10 19:00:39 +0700

    have to nice frontend website

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,26 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tasks Management</title>
-</head>
-<body>
-    <form action="{{ url('tasks') }}" method="post">
-        {{ csrf_field() }}
-        <input type="text" name="name">
-        <textarea name="description"></textarea>
-        <input type="submit" value="Create Task">
-    </form>
-    <h1>Tasks Management</h1>
-    <ul>
-        @foreach($tasks as $task)
-            <li>
-                {{ $task->name }} <br/>
-                {{ $task->description }}
-            </li>
-        @endforeach
-    </ul>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<h1 class="page-header text-center">Tasks Management</h1>
+<div class="row">
+    <div class="col-md-4 col-md-offset-2">
+        <h2>Tasks</h2>
+        <ul class="list-group">
+            @foreach ($tasks as $task)
+                <li class="list-group-item">
+                    {{ $task->name }} <br/>
+                    {{ $task->description }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-4">
+        <h2>New Task</h2>
+        <form action="{{ url('tasks') }}" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="name" class="control-label">Name</label>
+                <input id="name" class="form-control" type="text" name="name">
+            </div>
+            <div class="form-group">
+                <label for="description" class="control-label">Description</label>
+                <textarea id="description" class="form-control" name="description"></textarea>
+            </div>
+
+            <input type="submit" value="Create Task" class="btn btn-primary">
+        </form>
+    </div>
+</div>
+@endsection
